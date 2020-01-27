9b83f32738756ab83f0b788f2b981238f082fab4
Matt Palmerlee <mpalmerlee@gmail.com>
2020-01-27 12:28:51 -0800

    added api property to entity for swagger

--- src/todos/todo.entity.ts
+++ src/todos/todo.entity.ts
@@ -1,13 +1,16 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class Todo {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column({ length: 500 })
   title: string;
 
+  @ApiProperty()
   @Column()
   completed: boolean;
 }
